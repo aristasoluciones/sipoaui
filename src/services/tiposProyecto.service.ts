0dc4a1b2d96f79f5d7896ada011a28b94effdbc3
Brayan Alejandro Jimenez Gonzalez <Brayan_AlejandroJG@hotmail.com>
2026-02-20 11:47:19 -0600

    fix/cambio de ruta para tipos-proyecto

--- src/services/tiposProyecto.service.ts
+++ src/services/tiposProyecto.service.ts
@@ -3,7 +3,7 @@ import http from '@/src/lib/axios';
 export const TiposProyectoService = {
   async getAll() {
     try {
-      const response = await http.get('/api/tipos-proyecto');
+      const response = await http.get('/api/catalogos/tipos-proyecto');
       return response.data;
     } catch (error) {
       console.error('Error trayendo tipos proyecto:', error);
